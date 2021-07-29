d52f07d87925709444228ca6f48c47755d679846
MounikaPasam <78883313+MounikaPasam@users.noreply.github.com>
2021-07-28 22:04:38 -0400

    Create profile.py

---
+++ profile.py
@@ -0,0 +1,21 @@
+import geni.portal as portal
+import geni.rspec.pg as pg
+import geni.rspec.igext as IG
+
+pc = portal.Context()
+request = pc.makeRequestRSpec()
+
+tourDescription = \
+"""
+This profile provides a two-node for assignment-2. One node will be the Apache Webserver, and the other node is a client for NFS mount. 
+"""
+
+tour = IG.Tour()
+tour.Description(IG.Tour.TEXT,tourDescription)
+request.addTour(tour)
+prefixForIP = "192.168.1."
+link = request.LAN("lan")
+
+for i in range(2):
+  if i == 0:
+    node = request.X
